338b193ab6b3474677df62181294f330081088f1
Naufal Firoos <itsfiroos@gmail.com>
2025-01-19 20:04:13 +0700

    fix[accordion.tsx] : removing unused imports

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -2,10 +2,7 @@
 
 import * as React from "react";
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
-import { ChevronDown, ChevronDownIcon } from "lucide-react";
-
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 import CustomChevronDown from "@/app/components/chevron-down";
 
 const Accordion = AccordionPrimitive.Root;
