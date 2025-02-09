ca506e01446f8ab2459603b7b4edbde8f19de7ba
firoos18 <itsfiroos@gmail.com>
2025-02-09 21:53:38 +0700

    fix[topbar] : fixing topbar views in small breakpoints

--- app/components/topbar.tsx
+++ app/components/topbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const TopBar = () => {
@@ -33,6 +34,21 @@ const TopBar = () => {
         </button>
         <button className="rounded-full bg-black px-4 py-2 text-white">
           Contact Me
+        </button>
+      </div>
+      <div className="flex flex-row space-x-2 rounded-full border-[1px] border-[#D0D0D0] py-1 pl-1 pr-2 lg:hidden">
+        <div className="rounded-full bg-black px-4 py-2">
+          <p className="text-[12px] font-semibold text-white">Contact Me</p>
+        </div>
+        <button>
+          <Image
+            src={"/menu.png"}
+            alt="Menu Icon"
+            width={24}
+            height={24}
+            priority
+            style={{ objectFit: "contain" }}
+          />
         </button>
       </div>
     </div>
