086d6137fe098256978a367dc6395077563db372
firoos18 <itsfiroos@gmail.com>
2025-02-09 13:02:34 +0700

    fix[intro] : fixing intro according to the new design

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -19,15 +19,15 @@ const Intro = () => {
         fill
         priority
       />
-      <div className="px-4 lg:px-20 lg:mt-25 relative z-10 flex flex-col">
-        <p className="text-2xl sm:text-4xl lg:text-6xl w-[220px] lg:w-[440px] font-semibold">
+      <div className="px-4 lg:px-20 lg:mt-24 relative z-10 flex flex-col">
+        <p className="text-2xl sm:text-4xl lg:text-5xl w-[220px] lg:w-[440px] font-semibold">
           Hi! I’m Dewi.
         </p>
-        <p className="mt-4 text-base sm:text-xl lg:text-4xl lg:leading-relaxed lg:mt-12">
-          A <b>Finance</b> and <b>Business Expert</b> specializing in{" "}
-          <b>financial management</b> and <b>project coordination</b>. I help
-          entrepreneurs streamline operations, optimize finances, and manage
-          projects, so they can focus on growing their businesses.
+        <p className="mt-4 text-base sm:text-xl lg:text-2xl lg:leading-relaxed lg:mt-12">
+          With <b>over 20 years</b> of experience in finance and accounting, I
+          specialize in helping <b>mompreneurs</b> with service-based businesses
+          take control of their finances, build sustainable income, and achieve
+          financial confidence. 
         </p>
       </div>
       <div className="relative mt-24">
@@ -41,7 +41,7 @@ const Intro = () => {
             className="relative object-cover"
           />
         </div>
-        <p className="absolute top-0 lg:inset-y-1/2 lg:inset-x-2/4 w-64 lg:w-[720px] right-0 text-end mr-4 font-semibold text-base sm:text-lg lg:text-4xl lg:text-center">
+        <p className="absolute top-0 lg:inset-y-1/2 lg:inset-x-2/4 w-64 lg:w-[720px] right-0 text-center mr-4 font-semibold text-base sm:text-lg lg:text-3xl lg:text-start">
           My goal is to provide you with reliable support that drive growth and
           achieve <br /> financial goals.
         </p>
