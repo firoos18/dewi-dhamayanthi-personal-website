432c2c8ccbfbccca4fb2e617f109edd12844dd5f
firoos18 <itsfiroos@gmail.com>
2025-02-11 15:44:30 +0700

    fix[footer] : changing footer layout

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,21 +2,60 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="relative flex flex-col w-full min-h-96 lg:min-h-[440px] mt-14 px-4 lg:px-24 justify-center">
-      <Image
-        className="absolute inset-0 object-cover"
-        src={"/footer-bg.png"}
-        alt="background"
-        sizes="50vw"
-        fill
-        priority
-      />
-      <div className="relative z-10 flex flex-col lg:space-y-24">
-        <div className="lg:flex-col hidden lg:flex text-primary text-3xl font-bold">
+    <div className="mt-14 flex min-h-72 w-full flex-col justify-center px-4 lg:px-6">
+      <div className="relative mb-8 h-full min-h-72 w-full">
+        <Image
+          src={"/footer-bg.png"}
+          alt="background"
+          fill
+          priority
+          style={{ borderRadius: "16px" }}
+        />
+        <div className="absolute z-10 flex w-full flex-row justify-between px-6 pt-10">
+          <div className="flex h-full w-1/3 flex-col items-start">
+            <p className="h-1/6 w-1/4 text-lg font-bold leading-tight text-primary">
+              Dewi Dhamayanthi.
+            </p>
+          </div>
+          <div className="flex h-full w-1/3 flex-col leading-tight">
+            <p className="text-lg font-semibold">My Media</p>
+            <div className="mt-8 flex w-full flex-col space-y-3 text-sm font-semibold">
+              <div className="flex w-full flex-row space-x-2">
+                <p className="font-semibold">Instagram</p>
+                <p className="font-normal text-[#6A6A6A]">@dee_dhamayanthi</p>
+              </div>
+              <div className="flex w-full flex-row space-x-2">
+                <p className="font-semibold">Website</p>
+                <p className="font-normal text-[#6A6A6A]">
+                  dewidhamayanthi.com
+                </p>
+              </div>
+              <div className="flex w-full flex-row space-x-2">
+                <p className="font-semibold">LinkedIn</p>
+                <p className="font-normal text-[#6A6A6A]">Dewi Dhamayanti</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex h-full w-1/3 flex-col leading-tight">
+            <p className="text-lg font-semibold">Contacts</p>
+            <div className="mt-8 flex w-full flex-col space-y-3 text-sm font-semibold">
+              <div className="flex w-full flex-row space-x-2">
+                <p className="font-semibold">Email</p>
+                <p className="font-normal text-[#6A6A6A]">
+                  va.deedhamayanthi@gmail.com
+                </p>
+              </div>
+              <div className="flex w-full flex-row space-x-2">
+                <p className="font-semibold">Phone</p>
+                <p className="font-normal text-[#6A6A6A]">+62 819-0143-0494</p>
+              </div>
+            </div>
+          </div>
+          {/* <div className="hidden text-3xl font-bold text-primary lg:flex lg:flex-col">
           <p>Dewi</p>
           <p>Dhamayanthi.</p>
         </div>
-        <div className="flex flex-col lg:flex-row w-full space-y-8 lg:justify-between text-lg sm:text-xl lg:text-2xl">
+        <div className="flex w-full flex-col space-y-8 text-lg sm:text-xl lg:flex-row lg:justify-between lg:text-2xl">
           <div className="flex flex-col space-y-3">
             <p className="font-semibold">My Media</p>
             <div className="flex flex-row space-x-2">
@@ -43,6 +82,7 @@ const Footer = () => {
               <p className="text-[#6A6A6A]">+62 819-0143-0494</p>
             </div>
           </div>
+        </div> */}
         </div>
       </div>
     </div>
