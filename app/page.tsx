a9d134f64c14850cdcd6b5148b47ff057dc558f4
firoos18 <itsfiroos@gmail.com>
2025-04-27 23:28:32 +0700

    fix [token refresher] : removing token refresher

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import useTokenRefresher from "@/utils/hooks/useTokenRefresher";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  useTokenRefresher();
-
   redirect("/blog");
 }
