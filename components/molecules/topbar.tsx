31024032c022718ab961f2e523e5591a16a23bb9
firoos18 <itsfiroos@gmail.com>
2025-04-27 21:30:03 +0700

    feat [Blog/Post] : adding coming soon on /blog/post page

--- components/molecules/topbar.tsx
+++ components/molecules/topbar.tsx
@@ -23,16 +23,16 @@ const TopBar = () => {
         isScrolled ? "shadow-top-bar" : "shadow-sm"
       } z-50 flex flex-row items-center justify-between bg-background transition-shadow duration-300`}
     >
-      <div className="flex flex-col -space-y-1">
+      <Link href={"/"} className="flex flex-col -space-y-1">
         <p>Dewi</p>
         <p>Dhayamanthi.</p>
-      </div>
+      </Link>
       <div className="mr-5 hidden flex-row space-x-8 font-semibold text-black lg:flex lg:text-lg">
-        <Link href={"blog/ebook"} className="self-center">
-          Blog
+        <Link href={"/blog/post"} className="self-center">
+          Post
         </Link>
-        <Link href={"blog/ebook"} className="self-center">
-          EBook
+        <Link href={"/blog/ebook"} className="self-center">
+          Ebook
         </Link>
         <button className="rounded-full border-2 border-black px-4 py-2">
           Book a Call
